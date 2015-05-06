2c993f75a7df5037798f052ee4f1141bec16e263
Bookman <50573750@163.com>
2015-05-06 21:19:46 +0800

    It's a long time for TransGMPE to be convergence.

--- MultiChannelEmbedding/Embedding.cpp
+++ MultiChannelEmbedding/Embedding.cpp
@@ -12,15 +12,15 @@
 
 // 不放回采样。
 // Wordnet18 @ TransE : TransGMPE = 90.0% : 97.3%
-// Wordnet11 @ TransE : TransGMPE = 75.9% : 83.1% (83.6%)
+// Wordnet11 @ TransE : TransGMPE = 75.9% : 84.31%
 // Wordnet11 @ TransR : TransGGMPR = 85.5% : 85.7% (86.7%)
 // Freebase13 @ TransE : TransGMPE = 
 
 int main(int argc, char* argv[])
 {
 	//omp_set_num_threads(4);
-	EmbeddingModel*	Model = new TransGMPE(50, 0.02);
-	Model->run(1000);
+	EmbeddingModel*	Model = new TransGMPE(50, 0.02, 1);
+	Model->run(3000);
 
 	return 0;
 }
