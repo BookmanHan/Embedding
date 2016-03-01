1034736e945d0852838036c14c3d437f3216e42d
Xiaohan.Bookman <Bookman@vip.163.com>
2016-03-01 21:05:59 +0800

    For run.host

--- MultiChannelEmbedding/DetailedConfig.hpp
+++ MultiChannelEmbedding/DetailedConfig.hpp
@@ -41,3 +41,22 @@ const string triple_zeroshot_FB15K = "C:\\Data\\Knowledge Embedding\\FB15KZS\\ze
 const string semantic_vfile_FB15KZS = "C:\\Data\\Knowledge Embedding\\FB15KZS\\topics.bsd";
 const string semantic_tfile_FB15KZS = "C:\\Data\\Knowledge Embedding\\FB15KZS\\description.txt";
 #endif
+
+#ifdef LINUX_LOAD
+const Dataset FB15K("FB15K", "/home/bookman/data/Knowledge Embedding/FB15K/", "train.txt", "dev.txt", "test.txt", true);
+const Dataset FB13("FB13", "/home/bookman/data/Knowledge Embedding/FB13/", "train.txt", "dev.txt", "test.txt", false);
+const Dataset WN11("WN11", "/home/bookman/data/Knowledge Embedding/WN11/", "train.txt", "dev.txt", "test.txt", false);
+const Dataset WN18("WN18", "/home/bookman/data/Knowledge Embedding/WN18/", "train.txt", "dev.txt", "test.txt", true);
+const Dataset Wordnet("Wordnet", "/home/bookman/data/Knowledge Embedding/Wordnet/", "train.txt", "dev.txt", "test.txt", false);
+const Dataset Freebase("Freebase", "/home/bookman/data/Knowledge Embedding/Freebase/", "train.txt", "dev.txt", "test.txt", false);
+const string report_path = "D:\\สตั้\\Report\\Experiment.Embedding\\";
+const string semantic_vfile_FB15K = "/home/bookman/data/Knowledge Embedding/FB15K/topics.bsd";
+const string semantic_tfile_FB15K = "/home/bookman/data/Knowledge Embedding/FB15K/description.txt";
+const string semantic_vfile_WN18 = "/home/bookman/data/Knowledge Embedding/WN18/topics.bsd";
+const string semantic_tfile_WN18 = "/home/bookman/data/Knowledge Embedding/WN18/descriptions.txt";
+const string type_file_FB15K = "/home/bookman/data/Knowledge Embedding/FB15K/type.txt";
+const string type_file_FB15KZS = "/home/bookman/data/Knowledge Embedding/FB15KZS/type.txt";
+const string triple_zeroshot_FB15K = "/home/bookman/data/Knowledge Embedding/FB15KZS/zeroshot.txt";
+const string semantic_vfile_FB15KZS = "/home/bookman/data/Knowledge Embedding/FB15KZS/topics.bsd";
+const string semantic_tfile_FB15KZS = "/home/bookman/data/Knowledge Embedding/FB15KZS/description.txt";
+#endif
